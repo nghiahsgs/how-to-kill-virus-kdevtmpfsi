e00f830b03c721858d460741400dfaebc9edac01
nghia nguyen <futuregohan1997@gmail.com>
2021-04-06 10:28:41 +0700

    make it better

--- bash_cron.py
+++ bash_cron.py
@@ -19,14 +19,26 @@ def read_file(file_name):
     return ndung
 
 while True:
+    # username = 'www-data'
+    username = 'apache'
     os.system("ps -aux | grep kdevtmpfsi >a.txt")
     ndung = read_file('a.txt')
-    id_process = regex_one_value('www-data ([0-9]+) ',ndung)
+    id_process = regex_one_value('%s ([0-9]+) '%username,ndung)
     os.system('kill -9 %s'%id_process)
 
     os.system("ps -aux | grep kinsing >a.txt")
     ndung = read_file('a.txt')
-    id_process = regex_one_value('www-data ([0-9]+) ',ndung)
+    id_process = regex_one_value('%s ([0-9]+) '%username,ndung)
     os.system('kill -9 %s'%id_process)
+
+    os.system("ps -aux | grep kthreaddi >a.txt")
+    ndung = read_file('a.txt')
+    id_process = regex_one_value('%s ([0-9]+) '%username,ndung)
+    os.system('kill -9 %s'%id_process)
+
+    # xoa het cac file do virus sinh ra
+    os.system("rm -rf /tmp/kdevtmpfsi*")
+    os.system("rm -rf /tmp/kinsing*")
     
     time.sleep(30*60) #sleep 30p
+
